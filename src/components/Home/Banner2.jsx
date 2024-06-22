2bd691fd8547ba9378ba517c00bb81bdb486eb1a
hrSajib <harun15-4293@diu.edu.bd>
2024-06-22 22:45:54 +0600

    done

--- src/components/Home/Banner2.jsx
+++ src/components/Home/Banner2.jsx
@@ -1,8 +1,17 @@
 
+import Aos from "aos";
+import 'aos/dist/aos.css'
+import { useEffect } from "react";
+
+
 const Banner2 = () => {
+
+    useEffect(()=>{
+        Aos.init();
+      },[])
     return (
-        <div >
-            <img className="mx-auto lg:h-[700px] rounded-xl" src="https://i.ibb.co/T4Cfpzp/banner2.jpg" alt="" />
+        <div data-aos="fade-down" className=" h-[800px]">
+            <img className="mx-auto lg:h-[760px] rounded-xl" src="https://i.ibb.co/T4Cfpzp/banner2.jpg" alt="" />
             <div className="relative lg:left-[300px] lg:bottom-[600px]">
                 <p className="text-gray-300 lg:text-[120px]">ALL NEWS</p>
                 <p className="text-gray-300 lg:text-[80px]">Under Your </p>
--- src/components/Home/Banner3.jsx
+++ src/components/Home/Banner3.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+
+import Aos from "aos";
+import 'aos/dist/aos.css'
+import { useEffect } from "react";
+
+
+
+
 
 const Banner3 = () => {
+
+    useEffect(()=>{
+        Aos.init();
+      },[])
     return (
-        <div>
+        <div data-aos="fade-down" className="h-[900px]">
+            <img className='mx-auto rounded-xl' src="https://i.ibb.co/2FCKYqY/banner3.jpg" alt="Banner" />
+            <div className='flex gap-[200px]'>
+                <ul className='list-disc ml-5 text-5xl relative left-[300px] bottom-[600px] text-white'>
+                    <li className='bg-gray-500 p-5 rounded-t-2xl'>Top Stories</li>
+                    <li className='bg-gray-500 p-5'>World News</li>
+                    <li className='bg-gray-500 p-5'>National News</li>
+                    <li className='bg-gray-500 p-5'>Local News</li>
+                    <li className='bg-gray-500 p-5 rounded-b-2xl'>Business</li>
             
+                </ul>
+                <ul className='list-disc ml-5 text-5xl relative left-[300px] bottom-[600px] text-white'>
+                   
+                    <li className='bg-gray-500 p-5 rounded-t-2xl'>Technology</li>
+                    <li className='bg-gray-500 p-5'>Entertainment</li>
+                    <li className='bg-gray-500 p-5'>Sports</li>
+                    <li className='bg-gray-500 p-5'>Health</li>
+                    <li className='bg-gray-500 p-5 rounded-b-2xl'>Lifestyle</li>
+                </ul>
+            </div>
         </div>
     );
 };
